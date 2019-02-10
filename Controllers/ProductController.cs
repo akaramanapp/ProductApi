f44fcc7fc6400e02d65257ae9fbeb84c6071ad86
akaraman <akaraman@albarakaturk.com.tr>
2019-02-10 22:51:16 +0300

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET api/product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            List<Product> productList = new List<Product>(){
+                new Product(){
+                    Name="Klavye",
+                    Category="Donanım",
+                    Price= 120
+                },
+                new Product(){
+                    Name="Kulaklık",
+                    Category="Donanım",
+                    Price= 20
+                },
+                new Product(){
+                    Name="Microsoft Windows 95",
+                    Category="Lisans",
+                    Price= 499
+                }
+            };
+            return productList;
+        }
+    }
+
+    public class Product {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
